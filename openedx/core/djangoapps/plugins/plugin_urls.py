6d4e823f32c9907ed2da2bbb4fe7ff7dbf5e401d
Aarif <mraarif@outlook.com>
2020-01-17 23:31:21 +0500

    changes

--- openedx/core/djangoapps/plugins/plugin_urls.py
+++ openedx/core/djangoapps/plugins/plugin_urls.py
@@ -14,9 +14,8 @@ def get_patterns(project_type):
     return [
         url(
             url_config.get(constants.PluginURLs.REGEX, r''),
-            include((url_module_path, url_config.get(constants.PluginURLs.APP_NAME)),
-                    namespace=url_config[constants.PluginURLs.NAMESPACE],
-                    )
+            include((url_module_path, url_config.get(constants.PluginURLs.APP_NAME))),
+            namespace=url_config[constants.PluginURLs.NAMESPACE],
         )
         for url_module_path, url_config in _iter_plugins(project_type)
     ]
