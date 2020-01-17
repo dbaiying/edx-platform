746f07915ae6dc1bc4630a62761c79fcea221e6f
zia.fazal@arbisoft.com <zia.fazal@arbisoft.com>
2020-01-17 12:21:50 +0500

    Added ability to add enrollment with GlobalStaff permissions
    added unit test

--- openedx/core/djangoapps/enrollments/tests/test_views.py
+++ openedx/core/djangoapps/enrollments/tests/test_views.py
@@ -1000,6 +1000,36 @@ class EnrollmentTest(EnrollmentTestMixin, ModuleStoreTestCase, APITestCase, Ente
         self.assertTrue(is_active)
         self.assertEqual(course_mode, updated_mode)
 
+    @ddt.data(True, False)
+    def test_enrollment_with_global_staff_permissions(self, using_global_staff_user):
+        """Verify no audit enrollments for user different than requesting user and without
+        API_KEY should be done by the users having global staff permissions. """
+
+        CourseModeFactory.create(
+            course_id=self.course.id,
+            mode_slug=CourseMode.VERIFIED,
+            mode_display_name=CourseMode.VERIFIED,
+        )
+
+        if using_global_staff_user:
+            AdminFactory(username='global_staff', email='global_staff@example.com', password=self.PASSWORD)
+            self.client.login(username='global_staff', password=self.PASSWORD)
+        else:
+            self.client.login(username=self.OTHER_USERNAME, password=self.PASSWORD)
+
+        # Create an enrollment
+        self.assert_enrollment_status(
+            as_server=False,
+            mode=CourseMode.VERIFIED,
+            expected_status=status.HTTP_200_OK if using_global_staff_user else status.HTTP_404_NOT_FOUND
+        )
+
+        if using_global_staff_user:
+            course_mode, is_active = CourseEnrollment.enrollment_mode_for_user(self.user, self.course.id)
+            self.assertTrue(is_active)
+            self.assertEqual(course_mode, CourseMode.VERIFIED)
+        self.client.logout()
+
     @httpretty.activate
     @override_settings(ENTERPRISE_SERVICE_WORKER_USERNAME='enterprise_worker',
                        FEATURES=dict(ENABLE_ENTERPRISE_INTEGRATION=True))
